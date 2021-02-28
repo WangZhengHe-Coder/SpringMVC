28aac252f112c1aa64cecef03156322ae45c0a74
WangZhengHe-Coder <h641011815@163.com>
2021-02-28 10:54:49 +0800

    2021年02月28日

--- SpringMVC-05-json/src/main/java/com/wangzhenghe/controller/JsonTestController.java
+++ SpringMVC-05-json/src/main/java/com/wangzhenghe/controller/JsonTestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class JsonTestController {
 
     //String格式
+    //前端输出：User{name='??', age=12, sex='?'}
     @RequestMapping("/json1")
     @ResponseBody
     public String json1(){
@@ -22,9 +23,25 @@ public class JsonTestController {
     }
 
     //json格式
+    //前端输出：{"name":"??","age":12,"sex":"?"}
     @RequestMapping("/json2")
     @ResponseBody
     public String json2() throws JsonProcessingException {
+        //创建一个jackson的对象映射器，用来解析数据
+        ObjectMapper mapper = new ObjectMapper();
+        //创建一个对象
+        User user = new User("马云", 12, "男");
+        //将我们的对象解析成Json格式
+        String string = mapper.writeValueAsString(user);
+        return string;
+    }
+
+    //乱码解决：@RequestMapping(value = "/json1",produces ="application/json;charset=utf-8") 麻烦，不推荐
+    //json格式
+    //前端输出：{"name":"??","age":12,"sex":"?"}
+    @RequestMapping(value = "/json3",produces ="application/json;charset=utf-8")
+    @ResponseBody
+    public String json3() throws JsonProcessingException {
         //创建一个jackson的对象映射器，用来解析数据
         ObjectMapper mapper = new ObjectMapper();
         //创建一个对象
